bd8f16a05088d0b48a434756eef3493b028b6ed1
jessefrance <jesseluis09@gmail.com>
2022-10-04 23:49:56 -0300

    First Js

--- scripts/card-hover-animation.js
+++ scripts/card-hover-animation.js
@@ -1,6 +1,13 @@
-function addEventListenersToCards () {
+function handleMouseEnter(){
+    console.log("handle  mouse enter called", this);
+}
+function addEventListenersToCards() {
     const cardElements = document.getElementsByClassName('card');
-    console.log(cardElements)
+    
+    for (let index = 0; index < cardElements.length, index ++) {
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter)
+    }
 }
 
 document.addEventListener("DOMContentLoeaded", addEventListenersToCards, false);
