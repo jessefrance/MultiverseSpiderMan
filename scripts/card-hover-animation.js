8856c5d5c6ffd2ef913c98a9a58588a626effae4
jessefrance <jesseluis09@gmail.com>
2022-10-04 23:40:18 -0300

    add "card-hover.animat..."

---
+++ scripts/card-hover-animation.js
@@ -0,0 +1,6 @@
+function addEventListenersToCards () {
+    const cardElements = document.getElementsByClassName('card');
+    console.log(cardElements)
+}
+
+document.addEventListener("DOMContentLoeaded", addEventListenersToCards, false);
